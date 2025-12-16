706d1f6391871836a66756a50868175acc1b6e60
“rahmamahdi02” <119373093+rahmamahdi02@users.noreply.github.com>
2025-12-16 03:20:08 -0800

    feat: add in underline utility

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,12 +1,14 @@
 import { experiences } from "./experienceData";
 import { ExperienceCard } from "./ExperienceCard";
+import Underline from "./Underline";
+
 
 const Experience = () => {
   return (
     <section className="py-12 px-4">
       <h2 className="text-3xl font-bold font-mono text-center mb-8">Professional Experience</h2>
       <p className="text-muted-foreground max-w-2xl pb-10 text-center text-lg">
-      My recent work includes driving high-impact feature development at Sony Interactive Entertainment, delivering performance-optimized and accessible UI redesigns for Google’s Tech Equity Collective, and building scalable curriculum tools for open-source education initiatives used by 600+ developers.
+      My recent work includes <Underline>shipping UI features to 100+ million plus users at Playstation </Underline>(Sony Interactive Entertainment), delivering performance-optimized and accessible UI redesigns for Google’s Tech Equity Collective, and building scalable curriculum tools for <Underline>open-source education initiatives used by 600+ developers.</Underline>
         </p>
       <div className="space-y-4">
         {experiences.map((exp, index) => (
--- src/experienceData.ts
+++ src/experienceData.ts
@@ -12,7 +12,7 @@ export interface Experience {
 
 export const experiences: Experience[] = [
   {
-    company: "Playstaion (Sony Interactive Media)",
+    company: "Playstaion (Sony Interactive Entertainment)",
     companyLogo: "/images/logos/playstation.png",
     title: "Software Engineer (Contract)",
     duration: "Jul 2024 – Jul 2025",
@@ -74,7 +74,7 @@ export const experiences: Experience[] = [
     type: "1 year 2 months",
   },
   {
-    company: "UC Berkeley",
+    company: "EDGE in Tech™ Initiative at UC Berkeley",
     companyLogo: "/images/logos/ucberkeley.jpg",
     title: "Software Developer",
     duration: "Aug 2021 - May 2022",
