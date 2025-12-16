e66b35b1a96ce032930e9b6ca2cce9490ce63785
“rahmamahdi02” <119373093+rahmamahdi02@users.noreply.github.com>
2025-12-16 10:24:51 -0800

    feat: hero spacing

--- src/HeroSection.tsx
+++ src/HeroSection.tsx
@@ -4,73 +4,87 @@ import RotatingText from "./RotatingText/RotatingText";
 import { Button } from "./components/ui/button";
 import { Download, Link, Github } from "lucide-react";
 import { BackgroundBeams } from "./components/ui/background-beams";
-
 interface Props {
   children?: React.ReactNode;
 }
-
 const HeroSection = ({ children }: Props) => {
   return (
     <>
-  <main className="flex h-[80vh] flex-col items-center justify-center gap-10 p-5 sm:flex-row">
-        <div className="max-w-120">
-        <div className="max-w-120 mb-4">
-  <div className="inline-flex items-center rounded-full border-2 border-primary/50 bg-primary/10 px-4 py-1 text-sm font-medium text-primary shadow-glow">
-    <span className="relative mr-2 flex h-3 w-3">
-      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary/75 opacity-75"></span>
-      <span className="relative inline-flex h-3 w-3 rounded-full bg-primary"></span>
-    </span>
-    Open to opportunities
-  </div>
-</div>
-          <p className="text-foreground max-w-2xl font-mono text-lg">Hi, I'm Rahma M. 👋 </p>
-
-          <p className="text-foreground max-w-2xl font-mono text-lg">
-            Software Engineer experienced in shipping 
-            features to production and <Underline>scaling ML-powered systems used by 120M+ users.</Underline>
-            </p>
-            <p className="text-foreground max-w-2xl font-mono text-lg">           
+      <main className="flex h-[80vh] w-full flex-col items-center justify-center gap-10 px-5 sm:flex-row mx-auto max-w-7xl">
+        <div className="flex-1">
+          <div className="mb-4">
+            <div className="inline-flex items-center rounded-full border-2 border-primary/50 bg-primary/10 px-4 py-1 text-sm font-medium text-primary shadow-glow">
+              <span className="relative mr-2 flex h-3 w-3">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary/75 opacity-75"></span>
+                <span className="relative inline-flex h-3 w-3 rounded-full bg-primary"></span>
+              </span>
+              Open to opportunities
+            </div>
+          </div>
+          <h3 className="pb-15 text-3xl font-bold">
+            <RotatingText
+              texts={["Hello,", "Hola,", "Bonjour,", "Hi,", "Ciao,", "Merhaba,"]}
+              mainClassName="inline-block"
+              staggerFrom={"last"}
+              initial={{ y: "100%" }}
+              animate={{ y: 0 }}
+              exit={{ y: "-120%" }}
+              staggerDuration={0.025}
+              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+              transition={{ type: "spring", damping: 30, stiffness: 400 }}
+              rotationInterval={7000}
+            />{" "}
+          I'm Rahma M. 👋
+          </h3>
+          <p className="text-foreground font-mono text-lg">
+            Software Engineer experienced in shipping features to production and{" "}
+            <Underline>
+              scaling ML-powered systems used by 120M+ users.
+            </Underline>
             Experienced in end-to-end development of internal SDKs, full-stack
             applications, and developer tooling. Skilled in rapidly prototyping
             0→1 products, optimizing ML pipelines, and shipping reliable,
-            scalable systems in startup-like environments. Adept at <Underline>TypeScript,
-            Node.js, React Native (including bridgeless architecture), and
-            Python.</Underline> Passionate about developer experience, open-source
-            collaboration, developer-friendly APIs, and building robust,
-            user-facing systems from the ground up.
+            scalable systems in startup-like environments. Adept at{" "}
+            <Underline>
+              TypeScript, Node.js, React Native (including bridgeless
+              architecture), and Python.
+            </Underline>{" "}
+            Passionate about developer experience, open-source collaboration,
+            developer-friendly APIs, and building robust, user-facing systems
+            from the ground up.
           </p>
 
-          {/* two buttons to download my resume or check my github */}
           <div className="mt-5 flex flex-row gap-2">
-            <Button
-              asChild
-              className="group/btn group bg-primary text-primary-foreground hover:bg-primary/90 flex w-2/4 items-center justify-center gap-2 transition-all duration-200 hover:shadow-lg"
-            >
-              <a
-                href={"/resume.pdf"}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Download className="mr-2 h-4 w-4 transition-transform duration-200 group-hover:scale-135 group-hover:rotate-12" />
-                My Resume / CV
-              </a>
-            </Button>
+  <Button
+    asChild
+    className="group/btn group bg-primary text-primary-foreground hover:bg-primary/90 flex w-1/4 items-center justify-center gap-2 transition-all duration-200 hover:shadow-lg"
+  >
+    <a
+      href={"/resume.pdf"}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Download className="mr-2 h-4 w-4 transition-transform duration-200 group-hover:scale-135 group-hover:rotate-12" />
+      My Resume / CV
+    </a>
+  </Button>
 
-            <Button
-              variant={"outline"}
-              asChild
-              className="group flex w-1/4 items-center justify-center gap-2 hover:shadow-lg"
-            >
-              <a
-                href={"https://github.com/rahmamahdi02"}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Github className="mr-2 h-4 w-4 transition-transform duration-200 group-hover:scale-135 group-hover:rotate-12" />
-                Github
-              </a>
-            </Button>
-          </div>
+  <Button
+    variant={"outline"}
+    asChild
+    className="group flex w-1/4 items-center justify-center gap-2 hover:shadow-lg"
+  >
+    <a
+      href={"https://github.com/rahmamahdi02"}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Github className="mr-2 h-4 w-4 transition-transform duration-200 group-hover:scale-135 group-hover:rotate-12" />
+      Github
+    </a>
+  </Button>
+</div>
+
         </div>
       </main>
     </>
@@ -78,4 +92,3 @@ const HeroSection = ({ children }: Props) => {
 };
 
 export default HeroSection;
-
